0e522c1b155159d04572bb6110278895825bef3a
SAKTI BIMASENA <sbimasena@gmail.com>
2026-03-06 00:09:54 +0700

    refactor: change loss functions to class

--- src/loss.py
+++ src/loss.py
@@ -1,13 +1,22 @@
 import math
 
-def mse(predicted, actual):
-    vector = [(actual[i] - predicted[i])**2 for i in range(len(predicted))]
-    return sum(vector) / len(vector)
-
-def binary_cross_entropy(predicted, actual):
-    vector = [(actual[i] * math.log(predicted[i]) + (1 - actual[i]) * math.log(1 - predicted[i])) for i in range(len(predicted))]
-    return -sum(vector) / len(vector)
-
-def categorical_cross_entropy(predicted, actual):
-    vector = [actual[i] * math.log(predicted[i]) for i in range(len(predicted))]
-    return -sum(vector) / len(vector)
+class MSE:
+    def forward(self, predicted, actual):
+        vector = [(actual[i] - predicted[i])**2 for i in range(len(predicted))]
+        return sum(vector) / len(vector)
+    def derivative(self, predicted, actual):
+        return 2 * (predicted - actual) / len(predicted)
+    
+class BinaryCrossEntropy:
+    def forward(self, predicted, actual):
+        vector = [(actual[i] * math.log(predicted[i]) + (1 - actual[i]) * math.log(1 - predicted[i])) for i in range(len(predicted))]
+        return -sum(vector) / len(vector)
+    def derivative(self, predicted, actual):
+        return predicted - actual
+    
+class CategoricalCrossEntropy:
+    def forward(self, predicted, actual):
+        vector = [actual[i] * math.log(predicted[i]) for i in range(len(predicted))]
+        return -sum(vector) / len(vector)
+    def derivative(self, predicted, actual):
+        return predicted - actual
